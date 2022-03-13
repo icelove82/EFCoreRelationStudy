0055d4592e3d44130e7c6d489d8bce0f4fbb1490
YUN <yunmyeonghun@gmail.com>
2022-03-13 13:25:36 +0900

    Add project files.

---
+++ EFCoreRelationStudy/Data/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace EFCoreRelationStudy.Data
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string RpgClass { get; set; } = "Knight";
+
+        /* Relation 1:1 */
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ EFCoreRelationStudy/Data/User.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreRelationStudy.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+
+        /* Relation 1:n */
+        public List<Character> Characters { get; set; }
+    }
+}
