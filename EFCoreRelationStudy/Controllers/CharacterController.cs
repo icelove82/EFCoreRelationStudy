fb8ce059f476c31de30eca3bdb4b4b9fe24e9fc7
YUN <yunmyeonghun@gmail.com>
2022-03-13 20:02:23 +0900

    study 03 Add character controller

---
+++ EFCoreRelationStudy/Controllers/CharacterController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationStudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var characters = await _context.Characters
+                .Where(c => c.UserId == userId)
+                .Include(c => c.User)
+                .ToListAsync();
+
+            return Ok(characters);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(Character character)
+        {
+            var user = await _context.Users.FindAsync(character.UserId);
+            if (user == null)
+                return NotFound("User not found.");
+
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbCharacters(user.Id));
+        }
+
+        private async Task<List<Character>> GetDbCharacters(int userId)
+        {
+            return await _context.Characters
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
